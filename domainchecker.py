4bd18679f8f8127733c4708fd8f594464e2a2adf
Dmitry Malinovsky <damalinov@gmail.com>
2014-02-05 22:33:45 +0700

    Fixed issues about malformed output

--- domainchecker.py
+++ domainchecker.py
@@ -20,11 +20,11 @@ def bad_keyword(keyword):
 def read_keywords(filename):
     keywords = []
 
-    ignored = open("ignored.txt", "a")
+    ignored = open('ignored.txt', 'a')
 
-    with codecs.open(filename, "rU", "utf-16") as f:
-#    with codecs.open(filename, "rU", "utf-8") as f:
-        reader = csv.reader(f, delimiter="\t")
+    with codecs.open(filename, 'rU', 'utf-16') as f:
+#    with codecs.open(filename, 'rU', 'utf-8') as f:
+        reader = csv.reader(f, delimiter='\t')
         reader.next()  # skipping header
         for row in reader:
             keyword = row[1]
@@ -32,7 +32,7 @@ def read_keywords(filename):
                 keywords.append(row[1])
             else:
                 ignored.write(keyword)
-                ignored.write("\n")
+                ignored.write('\n')
 
         ignored.close()
 
@@ -40,10 +40,12 @@ def read_keywords(filename):
 
 
 def domainr_info_json(domainname):
-    requesturl = "http://www.domai.nr/api/json/info?q="
+    requesturl = 'http://www.domai.nr/api/json/info?q='
     requesturl += domainname
     request = urllib2.Request(requesturl)
-    request.add_header("User-Agent", "Mozilla/2.0 (compatible; MSIE 3.0; Windows 95)")
+    request.add_header(
+        'User-Agent', 'Mozilla/2.0 (compatible; MSIE 3.0; Windows 95)'
+    )
     opener = urllib2.build_opener()
     response = opener.open(request).read()
     objs = json.loads(response)
@@ -51,25 +53,24 @@ def domainr_info_json(domainname):
 
 
 def is_taken(domainr_json):
-    return domainr_json["availability"] == "taken"
+    return domainr_json['availability'] == 'taken'
 
 
 def concatenation(keyword, symbol):
-    return symbol.join(keyword.lower().split())
+    return symbol.join(keyword.split())
 
 
-def prepare_domains(keyword, tld):
-    domains = []
+def prepare_keywords(keyword):
+    keywords = []
     if ' ' in keyword:
         for symbol in ['', '-']:
-            domains.append(
-                '{0}{1}'.format(concatenation(keyword, symbol), tld)
-            )
+            keywords.append(concatenation(keyword, symbol))
     else:
-        domains.append('{0}{1}'.format(keyword, tld))
-    return domains
+        keywords.append(keyword)
+    return keywords
 
-def fetch_single(domain):
+
+def fetch_single(domain, row):
     try:
         req = domainr_info_json(domain)
     except Exception as e:
@@ -79,39 +80,39 @@ def fetch_single(domain):
 
     if is_taken(req):
         print(colored('Domain Taken --------- : {0}'.format(domain), 'red'))
-        return False
+        row.append('XXXXX')
     else:
         print(colored('Domain Free ---------- : {0}'.format(domain), 'green'))
-        return True
+        row.append(domain)
+
 
 def fetch_status(keywords):
-    rows = [["Keyword", "com", "net", "org", "com hyphen", "net hyphen", "org hyphen"]]
+    rows = [['Keyword', 'com', 'net', 'org', 'com hyphen', 'net hyphen', 'org hyphen']]
 
     for keyword in keywords:
+        is_singlekey = ' ' not in keyword
+        keyword = keyword.lower()
         row = [keyword]
-        for tld in [".com", ".net", ".org"]:
-            domains = prepare_domains(keyword, tld)
-            for domain in domains:
-                result = fetch_single(domain)
-                if result is True:
-                    row.append("XXXXX")
-                elif result is False:
-                    row.append(keyword)
+        for keyword in prepare_keywords(keyword):
+            for tld in ['.com', '.net', '.org']:
+                fetch_single('{0}{1}'.format(keyword, tld), row)
+        if is_singlekey:
+            row += ['XXXXX']*3
 
         rows.append(row)
     return rows
 
 
 def write_result(filename, rows):
-    with open(filename, "wb") as res:
-        writer = csv.writer(res, delimiter="\t")
+    with open(filename, 'wb') as res:
+        writer = csv.writer(res, delimiter='\t')
         writer.writerows(rows)
 
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(prog="domainchecker", version="0.2")
-    parser.add_argument("--input", "-i", type=str)
-    parser.add_argument("--output", "-o", type=str)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(prog='domainchecker', version='0.2')
+    parser.add_argument('--input', '-i', type=str)
+    parser.add_argument('--output', '-o', type=str)
 
     args = parser.parse_args()
 
